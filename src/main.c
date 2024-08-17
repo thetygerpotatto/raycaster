c8febd75a2bcfc6e406f1be3b3e427be138dd6c2
thetygerpotatto <letras.cj@gmail.com>
2024-08-16 19:39:17 -0500

    Colored walls implemented

--- src/main.c
+++ src/main.c
@@ -8,15 +8,16 @@
 typedef struct map {
     size_t rows;
     size_t cols;
-    bool **cells;
+    Color **cells;
 } map;
 
 void drawMiniMap();
 void drawPoint(Vector2 point, float r);
 Vector2 screenCordsToMapCords(Vector2 pos);
 void drawLine(Vector2 pos1, Vector2 pos2);
-bool** createMap(map m);
+Color** createMap(map m);
 bool isWhole(float n);
+bool cmpColor(Color a, Color b);
 Vector2 getClosest(Vector2 vec1, Vector2 vec2, Vector2 vec3);
 float snap(float p1, float sign);
 float sign(float x);
@@ -41,7 +42,7 @@ map gameMap = {.rows = mapSegments, .cols = mapSegments};
 float spacing_x;
 float spacing_y;
 
-Color backgrounColor = {0x45,0x45,0x45,0xff};
+Color backgroundColor = {0x45,0x45,0x45,0xff};
 Color wallColor = {0x19, 0x19, 0x19, 0xff};
 Color rayColor = {0xe5,0xbe,0x01,0xff};
 
@@ -56,52 +57,65 @@ int main(int argc, char ** argv) {
     float viewAngle = 0;
 
     Vector2 dir = {cosf(viewAngle), sinf(viewAngle)};
-    const float pov = PI/2;
+    const float fov = PI/2;
+    
+    bool render = true;
 
 
     InitWindow(screenWidth, screenHeight, "Raycast the Hell out of it");
     SetTargetFPS(60);
-
     for (size_t x = 0; x < gameMap.cols; x++) {
         for (size_t y = 0; y < gameMap.rows; y++) {
-            if (x == 0 || x == gameMap.cols-1 || y == 0 || y == gameMap.rows-1) gameMap.cells[x][y] = true;
+            gameMap.cells[x][y] = wallColor;
         }
     }
 
-    gameMap.cells[5][5] = true;
-    gameMap.cells[6][5] = true;
-    gameMap.cells[7][5] = true;
-    gameMap.cells[8][5] = true;
-    gameMap.cells[5][6] = true;
-    gameMap.cells[8][6] = true;
-    gameMap.cells[8][7] = true;
+    for (size_t x = 0; x < gameMap.cols; x++) {
+        for (size_t y = 0; y < gameMap.rows; y++) {
+            if (x == 0 || x == gameMap.cols-1 || y == 0 || y == gameMap.rows-1) gameMap.cells[x][y] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+        }
+    }
+
+    gameMap.cells[5][5] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+    gameMap.cells[6][5] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+    gameMap.cells[7][5] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+    gameMap.cells[8][5] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+    gameMap.cells[5][6] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+    gameMap.cells[8][6] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+    gameMap.cells[8][7] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+    gameMap.cells[8][9] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+    gameMap.cells[7][9] = (Color){rand()%255, rand()%255, rand()%255, 0xff};
+
 
 
     while (!WindowShouldClose()) {
         frameCount+=1;
 
-        //trace ray
-        seccondPoint = step(centerPoint, viewAngle);
-
         BeginDrawing();
-        ClearBackground(backgrounColor);
+        if (render) {
+            //tace ray
+            seccondPoint = step(centerPoint, viewAngle);
 
-        drawPov(centerPoint, pov, viewAngle, dir);
-        drawMiniMap();
+            ClearBackground(backgroundColor);
+
+            drawPov(centerPoint, fov, viewAngle, dir);
+            drawMiniMap();
 
 
-        drawPoint(centerPoint, 5.0f);
-        drawPoint(seccondPoint, 5.0f);
-        drawLine(centerPoint, seccondPoint);
+            drawPoint(centerPoint, 5.0f);
+            drawPoint(seccondPoint, 5.0f);
+            drawLine(centerPoint, seccondPoint);
 
+            render = false;
+        }
         EndDrawing();
+
         
         dir = (Vector2){cosf(viewAngle), sinf(viewAngle)};
 
         if (frameCount >= 1) {
             frameCount = 0;
-            if (IsKeyDown(KEY_W)) centerPoint = collision(Vector2Add(centerPoint, Vector2Scale(dir, 0.06f)),
-                                                          centerPoint);
+            if (IsKeyDown(KEY_W)) centerPoint = collision(Vector2Add(centerPoint, Vector2Scale(dir, 0.06f)), centerPoint);
             if (IsKeyDown(KEY_S)) centerPoint = collision(Vector2Subtract(centerPoint, Vector2Scale(dir, 0.06f)),
                                                           centerPoint);
             if (IsKeyDown(KEY_D)) centerPoint = collision(Vector2Add(centerPoint,
@@ -112,6 +126,8 @@ int main(int argc, char ** argv) {
                                                             , centerPoint);
             if (IsKeyDown(KEY_LEFT)) viewAngle -= PI/120.0f;
             if (IsKeyDown(KEY_RIGHT)) viewAngle += PI/120.0f;
+            if (IsKeyDown(KEY_W) || IsKeyDown(KEY_S) || IsKeyDown(KEY_D) ||
+                IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_RIGHT)) render = true;
         }
 
         
@@ -152,13 +168,12 @@ Vector2 step(Vector2 pc, float angle) {
         if (p2.x > 0 && p2.x < gameMap.cols &&
             p2.y > 0 && p2.x < gameMap.rows) {
 
-            if (gameMap.cells[(int)p2.x][(int)p2.y]) {
+            if (!cmpColor(gameMap.cells[(int)p2.x][(int)p2.y], wallColor)) {
                 return p2;
             }
         } 
         else 
         {
-
             return pc;
         }
     }
@@ -194,7 +209,7 @@ void drawMiniMap() {
 
     for (size_t cols = 0; cols < gameMap.cols; ++cols) {
         for (size_t rows = 0 ; rows < gameMap.rows; ++rows) {
-            if (gameMap.cells[cols][rows]) {
+            if (!cmpColor(gameMap.cells[cols][rows], wallColor)) {
                 DrawRectangle(miniMapCords.x + spacing_x*cols+1, miniMapCords.y + spacing_y*rows+1, spacing_x-2, spacing_y-2 , wallColor);
             }
         }
@@ -205,6 +220,7 @@ void drawPov(Vector2 pc, float fov, float angle, Vector2 dir) {
     for (int x = 0; x < screenWidth; ++x) {
         float currentAngle = (angle - fov / 2) + fov/screenWidth*x; 
         Vector2 p1 = step(pc, currentAngle);
+        drawLine(pc, p1);
         Vector2 v = Vector2Subtract(p1, pc);
         float distance = Vector2DotProduct(v, dir);
 
@@ -212,7 +228,7 @@ void drawPov(Vector2 pc, float fov, float angle, Vector2 dir) {
         float startDrawing = screenHeight/2 - wallHeight/2;
         float endDrawing = screenHeight/2 + wallHeight/2;
         
-        Color wallShade = {0, 0, 180/distance, 0xff};
+        Color wallShade = gameMap.cells[(int)p1.x][(int)p1.y];
         DrawLine(x, startDrawing, x, endDrawing, wallShade);
     }
 }
@@ -236,7 +252,7 @@ Vector2 screenCordsToMapCords(Vector2 pos) {
 Vector2 collision(Vector2 newcords, Vector2 oldCords) {
     if (newcords.x > 0 && newcords.x < gameMap.cols &&
         newcords.y > 0 && newcords.y < gameMap.rows) {
-        if (gameMap.cells[(int)newcords.x][(int)newcords.y]) {
+        if (!cmpColor(gameMap.cells[(int)newcords.x][(int)newcords.y], wallColor)) {
             return oldCords;
         } 
         else return newcords;
@@ -244,11 +260,11 @@ Vector2 collision(Vector2 newcords, Vector2 oldCords) {
     return oldCords;
 }
 
-bool** createMap(map m) {
-    bool** newMap;
-    newMap = calloc(m.rows, sizeof(bool**));
+Color** createMap(map m) {
+    Color** newMap;
+    newMap = calloc(m.rows, sizeof(Color**));
     for (size_t i = 0; i < m.rows; ++i) {
-        newMap[i] = calloc(m.cols, sizeof(bool));
+        newMap[i] = calloc(m.cols, sizeof(Color));
     }
     return newMap;
 }
@@ -260,6 +276,13 @@ bool isWhole(float n) {
     return false;
 }
 
+bool cmpColor(Color a, Color b) {
+    if (a.r != b.r) return false;
+    if (a.g != b.g) return false;
+    if (a.b != b.b) return false;
+    if (a.a != b.a) return false;
+    return true;
+}
 
 float sign(float x) {
     if (x > 0) return 1;
